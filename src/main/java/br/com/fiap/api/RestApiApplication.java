44524c6ac6af7e251dc19995d792197f8f74c3ba
Bruno Araujo <brunosousa357@gmail.com>
2024-04-23 20:43:31 -0300

    fix: changing column name to username

---
+++ src/main/java/br/com/fiap/api/RestApiApplication.java
@@ -0,0 +1,4 @@
+package br.com.fiap.api;
+
+public class RestApiApplication {
+}
--- src/main/java/br/com/fiap/api/model/Message.java
+++ src/main/java/br/com/fiap/api/model/Message.java
@@ -1,6 +1,6 @@
 package br.com.fiap.api.model;
 
-import jakarta.persistence.Column;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
@@ -8,6 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -20,18 +22,18 @@ import java.util.UUID;
 public class Message {
 
     @Id
+    @GenericGenerator(name="uuid")
     private UUID id;
 
-    @Column(nullable = false)
     @NotEmpty(message = "Usuário não pode estar vazio")
-    private String user;
+    private String username;
 
-    @Column(nullable = false)
     @NotEmpty(message = "Conteúdo não pode estar vazio")
     private String content;
 
-    @Builder.Default
-    private LocalDateTime creationMessageTime = LocalDateTime.now();
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSS")
+    private LocalDateTime creationMessageTime;
 
     @Builder.Default
     private int liked = 0;
